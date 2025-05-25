46423abb54f577b354284a19510c380995be0be9
matsufriends <pikapikan0526@gmail.com>
2025-05-25 16:06:29 +0900

    ShowIf/HideIfの調整とPingボタンの追加

--- MornEditorUtil.cs
+++ MornEditorUtil.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Reflection;
 using UnityEngine;
+using Object = UnityEngine.Object;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -83,6 +84,83 @@ namespace MornEditor
 
             value = false;
             return false;
+        }
+
+        public static void OnInspectorGUI(Object target, SerializedObject serializedObject)
+        {
+            var property = serializedObject.GetIterator();
+            property.NextVisible(true);
+            using (var changeCheck = new EditorGUI.ChangeCheckScope())
+            {
+                // 1行目のm_scriptと、そのpingボタン
+                using (new GUILayout.HorizontalScope())
+                {
+                    var cachedEnabled = GUI.enabled;
+                    GUI.enabled = false;
+                    EditorGUILayout.PropertyField(property);
+                    GUI.enabled = cachedEnabled;
+                    if (GUILayout.Button("Ping", GUILayout.Width(60)))
+                    {
+                        EditorGUIUtility.PingObject(target);
+                    }
+                }
+
+                while (property.NextVisible(false))
+                {
+                    DrawProperty(target, property);
+                }
+
+                if (changeCheck.changed)
+                {
+                    serializedObject.ApplyModifiedProperties();
+                }
+            }
+
+            var methods = target.GetType().GetMethods(
+                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.FlattenHierarchy);
+            foreach (var method in methods)
+            {
+                var buttonAttribute = method.GetCustomAttribute<ButtonAttribute>();
+                if (buttonAttribute != null)
+                {
+                    if (GUILayout.Button(method.Name))
+                    {
+                        method.Invoke(target, null);
+                    }
+                }
+
+                var onInspectorGUIAttribute = method.GetCustomAttribute<OnInspectorGUIAttribute>();
+                if (onInspectorGUIAttribute != null)
+                {
+                    method.Invoke(target, null);
+                }
+            }
+        }
+
+        private static void DrawProperty(Object target, SerializedProperty property)
+        {
+            var targetType = target.GetType();
+            var fieldInfo = targetType.GetField(
+                property.name,
+                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            if (fieldInfo != null)
+            {
+                var showIfAttribute = fieldInfo.GetCustomAttribute<ShowIfAttribute>();
+                var propertyName = showIfAttribute?.PropertyName;
+                if (showIfAttribute != null && TryGetBool(propertyName, property, out var show) && !show)
+                {
+                    return;
+                }
+
+                var hideIf = fieldInfo.GetCustomAttribute<HideIfAttribute>();
+                propertyName = hideIf?.PropertyName;
+                if (hideIf != null && TryGetBool(propertyName, property, out var hide) && hide)
+                {
+                    return;
+                }
+            }
+
+            EditorGUILayout.PropertyField(property, true);
         }
 #endif
     }
