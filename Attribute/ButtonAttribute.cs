a9c8e92a1c32b6134e476e2a3cccc7f5a8d7b9b8
matsufriends <pikapikan0526@gmail.com>
2025-06-22 14:58:19 +0900

    ButtonAttributeで引数を渡せるように

--- Attribute/ButtonAttribute.cs
+++ Attribute/ButtonAttribute.cs
@@ -4,5 +4,15 @@ namespace MornEditor
 {
     public sealed class ButtonAttribute : Attribute
     {
+        public string Name { get; }
+        
+        public ButtonAttribute()
+        {
+        }
+        
+        public ButtonAttribute(string name)
+        {
+            Name = name;
+        }
     }
 }
