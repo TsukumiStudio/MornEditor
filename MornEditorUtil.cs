ef4623ae710cebf7da6ed1125e7465ab68f78d32
matsufriends <pikapikan0526@gmail.com>
2025-06-25 03:36:24 +0900

    未使用関数の削除

--- MornEditorUtil.cs
+++ MornEditorUtil.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Reflection;
 using UnityEngine;
 using Object = UnityEngine.Object;
@@ -10,65 +9,6 @@ namespace MornEditor
 {
     public static class MornEditorUtil
     {
-        public struct MornEditorOption
-        {
-            public bool IsEnabled;
-            public bool IsIndent;
-            public bool IsBox;
-            public string Header;
-            public Color? Color;
-            public Color? BackgroundColor;
-            public bool ChangeColorIfEnabled;
-            public bool ChangeBackgroundColorIfEnabled;
-        }
-
-        public static void Draw(MornEditorOption option, Action action)
-        {
-            var cachedEnabled = GUI.enabled;
-            var cachedColor = GUI.color;
-            var cachedBackgroundColor = GUI.backgroundColor;
-            GUI.enabled = option.IsEnabled;
-            GUI.color = !option.ChangeColorIfEnabled || option.IsEnabled ? option.Color ?? cachedColor : cachedColor;
-            GUI.backgroundColor = !option.ChangeBackgroundColorIfEnabled || option.IsEnabled
-                ? option.BackgroundColor ?? cachedBackgroundColor : cachedBackgroundColor;
-            if (option.IsBox)
-            {
-                GUILayout.BeginVertical(GUI.skin.box);
-            }
-
-            if (!string.IsNullOrEmpty(option.Header))
-            {
-                // bold
-                var cachedFontStyle = GUI.skin.label.fontStyle;
-                GUI.skin.label.fontStyle = FontStyle.Bold;
-                GUILayout.Label(option.Header);
-                GUI.skin.label.fontStyle = cachedFontStyle;
-            }
-
-            if (option.IsIndent)
-            {
-                GUILayout.BeginHorizontal();
-                GUILayout.Space(20);
-                GUILayout.BeginVertical();
-            }
-
-            action?.Invoke();
-            if (option.IsIndent)
-            {
-                GUILayout.EndVertical();
-                GUILayout.EndHorizontal();
-            }
-
-            if (option.IsBox)
-            {
-                GUILayout.EndVertical();
-            }
-
-            GUI.enabled = cachedEnabled;
-            GUI.color = cachedColor;
-            GUI.backgroundColor = cachedBackgroundColor;
-        }
-
 #if UNITY_EDITOR
         internal static bool TryGetBool(string propertyName, SerializedProperty property, out bool value)
         {
