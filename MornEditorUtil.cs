e7995fb5925b05707342611e3b4887ffc27784f7
matsufriends <pikapikan0526@gmail.com>
2025-06-25 06:51:04 +0900

    ドラッグ対応

--- MornEditorUtil.cs
+++ MornEditorUtil.cs
@@ -210,9 +210,194 @@ namespace MornEditor
                 }
             }
 
-            EditorGUILayout.PropertyField(property, true);
+            // 配列/リストプロパティの場合、ドラッグ&ドロップを処理
+            if (property.isArray && property.propertyType != SerializedPropertyType.String)
+            {
+                DrawArrayPropertyWithDragAndDrop(property);
+            }
+            else
+            {
+                EditorGUILayout.PropertyField(property, true);
+            }
             
             GUI.enabled = previousEnabled;
+        }
+
+        private static void DrawArrayPropertyWithDragAndDrop(SerializedProperty property)
+        {
+            // BeginPropertyを使用してPropertyFieldの描画位置を正確に取得
+            var rect = EditorGUILayout.GetControlRect(true, EditorGUI.GetPropertyHeight(property, true));
+            
+            // ヘッダー部分のRectを計算（プロパティの最初の行）
+            var headerRect = new Rect(rect.x, rect.y, rect.width, EditorGUIUtility.singleLineHeight);
+            
+            // ドラッグ&ドロップの処理を先に行う（イベントが消費される前に）
+            if (property.arraySize >= 0 || property.arraySize == -1) // 空の配列も受け付ける
+            {
+                HandleArrayDragAndDrop(property, headerRect);
+            }
+            
+            // 通常のPropertyFieldを描画
+            EditorGUI.PropertyField(rect, property, true);
+        }
+
+        private static void HandleArrayDragAndDrop(SerializedProperty arrayProperty, Rect dropArea)
+        {
+            var evt = Event.current;
+            
+            // ドラッグオーバー時の処理
+            if (dropArea.Contains(evt.mousePosition))
+            {
+                if (evt.type == EventType.DragUpdated)
+                {
+                    // ドラッグされているオブジェクトが適切な型かチェック
+                    if (IsValidDraggedObjects(arrayProperty))
+                    {
+                        DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+                        evt.Use();
+                    }
+                }
+                else if (evt.type == EventType.DragPerform)
+                {
+                    // ドロップ時の処理
+                    if (IsValidDraggedObjects(arrayProperty))
+                    {
+                        DragAndDrop.AcceptDrag();
+                        
+                        // 配列要素の型を取得
+                        var elementType = GetArrayElementType(arrayProperty);
+                        
+                        // ドラッグされたオブジェクトを配列に追加
+                        foreach (var draggedObject in DragAndDrop.objectReferences)
+                        {
+                            if (draggedObject != null)
+                            {
+                                var objectToAdd = draggedObject;
+                                
+                                // GameObject がドラッグされていて、Component型の配列の場合
+                                if (elementType != null && 
+                                    typeof(Component).IsAssignableFrom(elementType) && 
+                                    draggedObject is GameObject gameObject)
+                                {
+                                    objectToAdd = gameObject.GetComponent(elementType);
+                                    if (objectToAdd == null)
+                                    {
+                                        continue;
+                                    }
+                                }
+                                
+                                var index = arrayProperty.arraySize;
+                                arrayProperty.InsertArrayElementAtIndex(index);
+                                var element = arrayProperty.GetArrayElementAtIndex(index);
+                                element.objectReferenceValue = objectToAdd;
+                            }
+                        }
+                        
+                        arrayProperty.serializedObject.ApplyModifiedProperties();
+                        evt.Use();
+                    }
+                }
+            }
+        }
+
+        private static bool IsValidDraggedObjects(SerializedProperty arrayProperty)
+        {
+            if (DragAndDrop.objectReferences.Length == 0)
+            {
+                return false;
+            }
+            
+            // 配列要素の型を取得
+            var elementType = GetArrayElementType(arrayProperty);
+            if (elementType == null)
+            {
+                // 型情報が取得できない場合は、ObjectReference型のプロパティであればOKとする
+                if (arrayProperty.arraySize > 0)
+                {
+                    var firstElement = arrayProperty.GetArrayElementAtIndex(0);
+                    return firstElement.propertyType == SerializedPropertyType.ObjectReference;
+                }
+                // 空の配列の場合は、とりあえず受け入れる
+                return true;
+            }
+            
+            // ドラッグされたすべてのオブジェクトが適切な型かチェック
+            foreach (var obj in DragAndDrop.objectReferences)
+            {
+                if (obj == null)
+                {
+                    continue;
+                }
+                
+                // 型の互換性をチェック
+                if (!elementType.IsAssignableFrom(obj.GetType()))
+                {
+                    // Componentの派生型の場合、GameObjectからGetComponentで取得可能かチェック
+                    if (typeof(Component).IsAssignableFrom(elementType) && obj is GameObject gameObject)
+                    {
+                        if (gameObject.GetComponent(elementType) == null)
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            
+            return true;
+        }
+        
+        private static System.Type GetArrayElementType(SerializedProperty arrayProperty)
+        {
+            // プロパティパスから親オブジェクトとフィールド情報を取得
+            var parentObject = GetActualObjectForProperty(arrayProperty);
+            if (parentObject == null)
+            {
+                return null;
+            }
+            
+            // フィールド名を取得（Array.data[n]形式を考慮）
+            var fieldName = arrayProperty.name;
+            if (arrayProperty.propertyPath.Contains(".Array.data["))
+            {
+                var lastDotIndex = arrayProperty.propertyPath.LastIndexOf(".Array.data[");
+                if (lastDotIndex > 0)
+                {
+                    var parentPath = arrayProperty.propertyPath.Substring(0, lastDotIndex);
+                    var parts = parentPath.Split('.');
+                    fieldName = parts[parts.Length - 1];
+                }
+            }
+            
+            var fieldInfo = parentObject.GetType().GetField(
+                fieldName,
+                BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            
+            if (fieldInfo != null)
+            {
+                var fieldType = fieldInfo.FieldType;
+                
+                // 配列型の場合
+                if (fieldType.IsArray)
+                {
+                    return fieldType.GetElementType();
+                }
+                
+                // List<T>型の場合
+                if (fieldType.IsGenericType)
+                {
+                    var genericTypeDef = fieldType.GetGenericTypeDefinition();
+                    if (genericTypeDef == typeof(System.Collections.Generic.List<>))
+                    {
+                        return fieldType.GetGenericArguments()[0];
+                    }
+                }
+            }
+            
+            return null;
         }
 #endif
     }
