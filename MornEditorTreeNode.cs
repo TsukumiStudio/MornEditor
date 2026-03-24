eea2b1c23fca4e3aa8617b47a4755a188ea59db2
matsufriends <pikapikan0526@gmail.com>
2026-03-24 14:01:41 +0900

    パス外のファイルは警告なしでスキップ

--- MornEditorTreeNode.cs
+++ MornEditorTreeNode.cs
@@ -41,7 +41,6 @@ namespace MornEditor
                 var path = _tree.NodeToPath(target);
                 if (!string.IsNullOrEmpty(_originalPath) && !path.StartsWith(_originalPath))
                 {
-                    MornEditorGlobal.LogWarning($"パス[{path}]は、指定したパス[{_originalPath}]で始まっていません。");
                     return;
                 }
                 var pathFromPrefix = path.Substring(_originalPath.Length);
